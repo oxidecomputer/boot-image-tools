ab61443eae8c99b17aac82cbedb9a0c917828f57
Joshua M. Clulow <jmc@oxide.computer>
2023-01-08 18:50:58 -0800

    allow target size override with mkimage -s

--- src/bin/mkimage.rs
+++ src/bin/mkimage.rs
@@ -1,6 +1,6 @@
 use std::os::unix::prelude::FileExt;
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use sha2::Digest;
 
 use bootserver::diskimage;
@@ -10,6 +10,7 @@ fn main() -> Result<()> {
     opts.reqopt("i", "", "input raw image file", "RAW");
     opts.reqopt("o", "", "output image file with header", "IMAGE");
     opts.optopt("O", "", "output boot_image_csum file", "CSUM_FILE");
+    opts.optopt("s", "", "target ramdisk size (MiB)", "MEBIBYTES");
 
     let a = opts.parse(std::env::args().skip(1))?;
 
@@ -19,6 +20,11 @@ fn main() -> Result<()> {
         .truncate(true)
         .write(true)
         .open(&a.opt_str("o").unwrap())?;
+    let target_size = if let Some(arg) = a.opt_str("s") {
+        arg.parse::<u64>()? * 1024 * 1024
+    } else {
+        4 * 1024 * 1024 * 1024
+    };
 
     let mut sum = sha2::Sha256::new();
     let mut buf = vec![0u8; 128 * 1024];
@@ -42,9 +48,18 @@ fn main() -> Result<()> {
         total += sz;
     }
 
+    let image_size: u64 = total.try_into().unwrap();
+    if image_size > target_size {
+        bail!(
+            "target size of {} bytes is less than image size of {}",
+            target_size,
+            image_size,
+        );
+    }
+
     let h = diskimage::Header {
-        image_size: total.try_into().unwrap(),
-        target_size: 4 * 1024 * 1024 * 1024, /* XXX */
+        image_size,
+        target_size,
         dataset_name: "rpool/ROOT/ramdisk".into(), /* XXX */
         sha256: sum.finalize().into(),
     };
