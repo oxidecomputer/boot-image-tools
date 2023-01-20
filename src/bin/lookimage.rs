1b77d54aa192801f156e505c9d8309a0c18cc5f7
Andy Fiddaman <andy@omnios.org>
2023-01-20 19:19:21 +0000

    format v2: support for compressed images

--- src/bin/lookimage.rs
+++ src/bin/lookimage.rs
@@ -1,3 +1,5 @@
+use flate2::read::ZlibDecoder;
+use std::io::{Read, Seek, SeekFrom};
 use std::os::unix::prelude::FileExt;
 
 use anyhow::{bail, Result};
@@ -14,7 +16,7 @@ fn main() -> Result<()> {
         bail!("which image file do you want to inspect?");
     }
 
-    let fi = std::fs::File::open(&a.free[0])?;
+    let mut fi = std::fs::File::open(&a.free[0])?;
 
     /*
      * Read the image header first.
@@ -22,6 +24,23 @@ fn main() -> Result<()> {
     let mut buf = vec![0u8; diskimage::DISK_HEADER_LENGTH];
     fi.read_exact_at(&mut buf, 0)?;
     let h = diskimage::Header::from_bytes(&buf)?;
+    let hsum = h.checksum_str();
+
+    println!("flags = {:#x} ({:?})", h.flags.bits(), h.flags);
+    println!("data size = {}", h.data_size);
+    println!("image size = {}", h.image_size);
+    println!("target size = {}", h.target_size);
+    println!("image sum = {}", hsum);
+    println!("dataset name = {}", h.dataset_name);
+    if h.data_size == 0 {
+        bail!("data size is {}", h.data_size);
+    }
+    if h.image_size < h.data_size {
+        bail!("image size {} < data size {}", h.image_size, h.data_size);
+    }
+    if h.flags.contains(diskimage::Flags::COMPRESSED) {
+        println!("ratio = {:.2}x", h.image_size as f64 / h.data_size as f64);
+    }
 
     /*
      * Confirm that the data portion of the image matches the checksum in the
@@ -30,21 +49,40 @@ fn main() -> Result<()> {
     let mut sum = sha2::Sha256::new();
     let mut buf = vec![0u8; 128 * 1024];
     let mut total = 0usize;
-    loop {
-        let inpos = {
-            let total: u64 = total.try_into().unwrap();
-            total
-                .checked_add(diskimage::DISK_HEADER_LENGTH.try_into().unwrap())
-                .unwrap()
-        };
-        let sz = fi.read_at(&mut buf, inpos)?;
-        if sz == 0 {
-            break;
+
+    if h.flags.contains(diskimage::Flags::COMPRESSED) {
+        fi.seek(SeekFrom::Start(diskimage::DISK_HEADER_LENGTH as u64))?;
+        let mut decoder = ZlibDecoder::new(fi);
+
+        loop {
+            let sz = decoder.read(&mut buf)?;
+            if sz == 0 {
+                break;
+            }
+
+            sum.update(&buf[0..sz]);
+
+            total += sz;
         }
+    } else {
+        loop {
+            let inpos = {
+                let total: u64 = total.try_into().unwrap();
+                total
+                    .checked_add(
+                        diskimage::DISK_HEADER_LENGTH.try_into().unwrap(),
+                    )
+                    .unwrap()
+            };
+            let sz = fi.read_at(&mut buf, inpos)?;
+            if sz == 0 {
+                break;
+            }
 
-        sum.update(&buf[0..sz]);
+            sum.update(&buf[0..sz]);
 
-        total += sz;
+            total += sz;
+        }
     }
 
     if h.image_size != total.try_into().unwrap() {
@@ -57,17 +95,10 @@ fn main() -> Result<()> {
 
     let sum =
         sum.finalize().iter().map(|x| format!("{:02x}", x)).collect::<String>();
-    let hsum =
-        h.sha256.iter().map(|x| format!("{:02x}", x)).collect::<String>();
 
     if sum != hsum {
         bail!("image sum {} does not match sum from header {}", sum, hsum);
     }
-
-    println!("image sum = {}", sum);
-    println!("image size = {}", h.image_size);
-    println!("target size = {}", h.target_size);
-    println!("dataset name = {}", h.dataset_name);
 
     Ok(())
 }
